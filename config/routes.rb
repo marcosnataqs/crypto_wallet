f2d3cb125c316a0a0b9d46c4886be0bbcbb69fca
marcosnataqs <marcosnataqs@gmail.com>
2022-08-28 11:23:45 -0300

    feat: image tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'welcome/index'
   resources :coins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
 end
